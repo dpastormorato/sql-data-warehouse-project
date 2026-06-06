4697b55cf212436852fc88c445a44ed3a66f7827
David Pastor Morato <121526305+dpastormorato@users.noreply.github.com>
2026-06-06 11:37:06 +1000

    Create procedure to load data into Bronze layer
    This procedure loads data into various Bronze layer tables from CSV files, handling errors and logging load durations.

---
+++ scripts/bronze/proc_load_bronze.sql
@@ -0,0 +1,126 @@
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+	
+	BEGIN TRY
+		SET @batch_start_time = GETDATE();
+		PRINT '===================================================================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '===================================================================================';
+	
+		PRINT '-----------------------------------------------------------------------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '-----------------------------------------------------------------------------------';
+	
+		
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_cust_info';
+		TRUNCATE TABLE bronze.crm_cust_info;
+	
+		PRINT '>> Inserting Data Into Table: bronze.crm_cust_info';
+		BULK INSERT bronze.crm_cust_info
+		FROM 'F:\Private\Library\Udemy\Building a Modern Data Warehouse - Data Engineering Bootcamp\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -----------'
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_prd_info';
+		TRUNCATE TABLE bronze.crm_prd_info;
+
+		PRINT '>> Inserting Data Into Table: bronze.crm_prd_info';
+		BULK INSERT bronze.crm_prd_info
+		FROM 'F:\Private\Library\Udemy\Building a Modern Data Warehouse - Data Engineering Bootcamp\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -----------'
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_sales_details';
+		TRUNCATE TABLE bronze.crm_sales_details;
+
+		PRINT '>> Inserting Data Into Table: bronze.crm_sales_details';
+		BULK INSERT bronze.crm_sales_details
+		FROM 'F:\Private\Library\Udemy\Building a Modern Data Warehouse - Data Engineering Bootcamp\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -----------'
+
+		PRINT '-----------------------------------------------------------------------------------';
+		PRINT 'Loading ERP Tables';
+		PRINT '-----------------------------------------------------------------------------------';
+		
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_cust_az12';
+		TRUNCATE TABLE bronze.erp_cust_az12;
+
+		PRINT '>> Inserting Data Into Table: bronze.erp_cust_az12';
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'F:\Private\Library\Udemy\Building a Modern Data Warehouse - Data Engineering Bootcamp\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -----------'
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_loc_a101';
+		TRUNCATE TABLE bronze.erp_loc_a101;
+
+		PRINT '>> Inserting Data Into Table: bronze.erp_loc_a101';
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'F:\Private\Library\Udemy\Building a Modern Data Warehouse - Data Engineering Bootcamp\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -----------'
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: erp_px_cat_g1v2';
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+		PRINT '>> Inserting Data Into Table: bronze.erp_px_cat_g1v2';
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'F:\Private\Library\Udemy\Building a Modern Data Warehouse - Data Engineering Bootcamp\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -----------'
+		SET @batch_end_time = GETDATE();
+		PRINT '>> Entire batch load duration: ' + CAST(DATEDIFF(second, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+	END TRY
+	BEGIN CATCH
+		PRINT '===================================================================================';
+		PRINT 'Error occurred during loading Bronze Layer.';
+		PRINT 'Error Message: ' + ERROR_MESSAGE();
+		PRINT 'Error Number: ' + CAST(ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error State: ' + CAST(ERROR_STATE() AS NVARCHAR);
+		PRINT '===================================================================================';
+	END CATCH
+END
