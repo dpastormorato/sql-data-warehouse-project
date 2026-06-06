36efde7c77d2626e91a538af64fb0616c7ebe65b
David Pastor Morato <121526305+dpastormorato@users.noreply.github.com>
2026-06-06 13:07:08 +1000

    Fix column name casing for dwh_create_date

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -21,7 +21,7 @@ CREATE TABLE silver.crm_cust_info (
     cst_marital_status  NVARCHAR(50),
     cst_gndr            NVARCHAR(50),
     cst_create_date     DATE,
-    dwh_create_Date     DATETIME2 DEFAULT GETDATE()
+    dwh_create_date     DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -37,7 +37,7 @@ CREATE TABLE silver.crm_prd_info (
     prd_line     NVARCHAR(50),
     prd_start_dt DATETIME,
     prd_end_dt   DATETIME,
-    dwh_create_Date     DATETIME2 DEFAULT GETDATE()
+    dwh_create_date     DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -55,7 +55,7 @@ CREATE TABLE silver.crm_sales_details (
     sls_sales    INT,
     sls_quantity INT,
     sls_price    INT,
-    dwh_create_Date     DATETIME2 DEFAULT GETDATE()
+    dwh_create_date     DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -66,7 +66,7 @@ GO
 CREATE TABLE silver.erp_loc_a101 (
     cid    NVARCHAR(50),
     cntry  NVARCHAR(50),
-    dwh_create_Date     DATETIME2 DEFAULT GETDATE()
+    dwh_create_date     DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -78,7 +78,7 @@ CREATE TABLE silver.erp_cust_az12 (
     cid    NVARCHAR(50),
     bdate  DATE,
     gen    NVARCHAR(50),
-    dwh_create_Date     DATETIME2 DEFAULT GETDATE()
+    dwh_create_date     DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -91,6 +91,6 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     cat          NVARCHAR(50),
     subcat       NVARCHAR(50),
     maintenance  NVARCHAR(50),
-    dwh_create_Date     DATETIME2 DEFAULT GETDATE()
+    dwh_create_date     DATETIME2 DEFAULT GETDATE()
 );
 GO
